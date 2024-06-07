e7fd8b2c05ce3cc86317d6491684c6c7f4c8a6c9
squach90 <160175686+squach90@users.noreply.github.com>
2024-06-07 17:35:44 +0200

    Fix scrolling and name

--- app/_landing/CalCom.tsx
+++ app/_landing/CalCom.tsx
@@ -5,17 +5,20 @@ import { Booker } from "@calcom/atoms";
 import { CalComEmbed } from "./CalComEmbed";
 
 export const CalCom = () => {
-    return (
+  return (
     <>
-        <Section classNames="mb-0 md:mb-0 lg:mb-0">
-            <h2 className="text-3xl lg:text-4xl font-bold">Schedule a call with me</h2>
-            <p className="text-lg text-muted-foreground mt-2 lg:mt-4">
-                30 minutes to talk about your futur on YouTube
-            </p>
-        </Section>
-        <Section classNames="max-w-4xl mt-8 p-0 md:mt-8 lg:mt-2">
-            <CalComEmbed />
-        </Section>
+      <Section classNames="mb-0 md:mb-0 lg:mb-0">
+        <h2 className="text-3xl lg:text-4xl font-bold">
+          Schedule a call with me
+        </h2>
+        <p className="text-lg text-muted-foreground mt-2 lg:mt-4">
+          30 minutes to talk about your futur on YouTube
+        </p>
+      </Section>
+      <div id="calcom"></div>
+      <Section classNames="max-w-4xl mt-8 p-0 md:mt-8 lg:mt-2">
+        <CalComEmbed />
+      </Section>
     </>
-    );
-}
+  );
+};
--- app/_landing/Hero.tsx
+++ app/_landing/Hero.tsx
@@ -1,36 +1,66 @@
-import { PropsWithChildren } from "react";
+"use client";
+
+import { PropsWithChildren, useState } from "react";
 import { Section } from "./Section";
 import classNames from "classnames";
 import { CustomIcons } from "./icons/CustomIcon";
-import { Button } from "@/components/ui/button"
-
+import { Button } from "@/components/ui/button";
+import "../_landing/App.css";
 
 export const Hero = () => {
-    return (
-        <Section classNames="flex flex-col gap-2 lg:gap-4">
-            <h1 className="text-4xl lg:text-5xl font-bold">Hey, I am Jules 👋</h1>
-            <p className="text-muted-foreground">I am{" "}<Code className="whitespace-nowrap"><CustomIcons className="inline mb-0.5 mr-0.5" name="youtube" size={18} />Youtuber</Code>{" "} and I created a video about <Code>meme</Code> and <Code> aviation </Code></p>
+  const [isScrolling, setIsScrolling] = useState(false);
 
-            <div className="mt-4 lg:mt-6 flex items-center flex-wrap gap-4">
-                <Button size="lg" className="w-fit">
-                    Schedule a call
-                </Button>
-                <Button size="lg" variant="ghost" className="w-fit">
-                    <CustomIcons name="twitter" size={20} className="mr-2 text-foreground" />
-                    @c3a5m7
-                </Button>
-            </div>
+  const scrollToSection = () => {
+    setIsScrolling(true);
+    const section = document.getElementById("calcom");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+    setTimeout(() => {
+      setIsScrolling(false);
+    }, 1000); // Durée de l'animation en millisecondes
+  };
 
-        </Section>
-    );
+  return (
+    <Section classNames="flex flex-col gap-2 lg:gap-4">
+      <h1 className="text-4xl lg:text-5xl font-bold">Hey, I am c3a5m7 👋</h1>
+      <p className="text-muted-foreground">
+        I am{" "}
+        <Code className="whitespace-nowrap">
+          <CustomIcons
+            className="inline mb-0.5 mr-0.5"
+            name="youtube"
+            size={18}
+          />
+          Youtuber
+        </Code>{" "}
+        and I created a video about <Code>meme</Code> and{" "}
+        <Code> aviation </Code>
+      </p>
+
+      <div className="mt-4 lg:mt-6 flex items-center flex-wrap gap-4">
+        <Button onClick={scrollToSection} size="lg" className="w-fit">
+          Schedule a call
+        </Button>
+        <Button size="lg" variant="ghost" className="w-fit">
+          <CustomIcons
+            name="twitter"
+            size={20}
+            className="mr-2 text-foreground"
+          />
+          @c3a5m7
+        </Button>
+      </div>
+    </Section>
+  );
 };
 
-export const Code = (props: PropsWithChildren<{ className?: string }> ) => {
-    return (
-        <span className="px-1 rounded-md inline-flex text-foreground items-center -mx-0.5 bg-accent/20 border-accent border py-1">
-            {props.children}
-        </span>
-    )
-}
+export const Code = (props: PropsWithChildren<{ className?: string }>) => {
+  return (
+    <span className="px-1 rounded-md inline-flex text-foreground items-center -mx-0.5 bg-accent/20 border-accent border py-1">
+      {props.children}
+    </span>
+  );
+};
 
 export default Hero;
