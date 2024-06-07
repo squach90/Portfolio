e2b63944a526290167a7d4af3b96b410b2dea26c
squach90 <160175686+squach90@users.noreply.github.com>
2024-06-07 17:56:51 +0200

    Add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import { Footer } from "./_landing/Footer";
 export default function Home() {
   return (
     <main className="h-full">
+      <title>c3a5m7</title>
+      <link rel="icon" href="/app/_landing/icons/c3a5m7_profile.jpeg" />
       <div
         className="absolute inset-x-0 top-10 -z-10 transform-gpu overflow-hidden blur-3xl"
         aria-hidden="true"
@@ -15,7 +17,8 @@ export default function Home() {
         <div
           className="relative aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#8cbe9c] to-[#2518d4] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           style={{
-            clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
         ></div>
       </div>
